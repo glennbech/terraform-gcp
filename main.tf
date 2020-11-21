9c626629b703e8bff02862ba2c3299c5367b0abf
ali iqbal <kamranali95@hotmail.com>
2020-11-21 03:34:43 +0100

    added logz_io

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ resource "google_cloud_run_service" "default" {
     spec {
       containers {
         image = "gcr.io/devops-gcp-project-296222/devops-gcp-project@sha256:ccf94a7865e0073893cbffa2f02d1475bba8899daf5cba3527e817d0a7c5661e"
+        env {
+          name = "LOGZ_TOKEN"
+          value = var.logz_token
+        }
       }
     }
   }
@@ -28,9 +32,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
+  location = google_cloud_run_service.default.location
+  project = google_cloud_run_service.default.project
+  service = google_cloud_run_service.default.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
