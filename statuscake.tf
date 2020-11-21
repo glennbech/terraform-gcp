a027b893995dff884d202f7e76164aa4d873fddb
iqbkam18 <kamranali95@hotmail.com>
2020-11-21 19:35:34 +0100

    lets try with status cake

--- statuscake.tf
+++ statuscake.tf
@@ -4,8 +4,8 @@ provider "statuscake" {
 
 resource "statuscake_test" "googlecloudruntest" {
   website_name = "My test"
-  website_url  = google_cloud_run_service.default.status[0].url
-  test_type    = "HTTP"
-  check_rate   = 300
+  website_url = google_cloud_run_service.default.status[0].url
+  test_type = "HTTP"
+  check_rate = 300
   contact_group = ["195492"]
 }
