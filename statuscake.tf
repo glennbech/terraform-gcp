626343d3d314cbcae32bbe81b0d947196332de83
iqbkam18 <kamranali95@hotmail.com>
2020-11-21 20:46:51 +0100

    lets try with status cake again

--- statuscake.tf
+++ statuscake.tf
@@ -1,9 +1,10 @@
 provider "statuscake" {
   username = "kamranali95hotmailcom"
+  apikey = var.status_cake
 }
 
 resource "statuscake_test" "googlecloudruntest" {
-  website_name = "My test"
+  website_name = "Test"
   website_url = google_cloud_run_service.default.status[0].url
   test_type = "HTTP"
   check_rate = 300
