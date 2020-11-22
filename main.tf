491db24c303e8d9dcd3ee0e0aaca365e6ab7cd47
ali iqbal <kamranali95@hotmail.com>
2020-11-22 21:52:42 +0100

    memory

--- main.tf
+++ main.tf
@@ -7,17 +7,14 @@ resource "google_cloud_run_service" "default" {
     spec {
       containers {
         image = "gcr.io/devops-gcp-project-296222/devops-gcp-project@sha256:992c6d8ae3083322541afdbde112bff79bf5e69c50795b6a4342ca8479c9c26e"
-        resources {
-          limits = {
-            memory = "512Mi"
-          }
-          env {
-            name = "LOGZ_TOKEN"
-            value = var.logz_token
-          }
+        env {
+          name = "LOGZ_TOKEN"
+          value = var.logz_token
+
         }
       }
     }
+  }
 
 
     traffic {
@@ -26,20 +23,20 @@ resource "google_cloud_run_service" "default" {
     }
   }
 
+}
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
   }
-  data "google_iam_policy" "noauth" {
-    binding {
-      role = "roles/run.invoker"
-      members = [
-        "allUsers",
-      ]
-    }
-  }
+}
 
-  resource "google_cloud_run_service_iam_policy" "noauth" {
-    location = google_cloud_run_service.default.location
-    project = google_cloud_run_service.default.project
-    service = google_cloud_run_service.default.name
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.default.location
+  project = google_cloud_run_service.default.project
+  service = google_cloud_run_service.default.name
 
-    policy_data = data.google_iam_policy.noauth.policy_data
-  }
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
